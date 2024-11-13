4b861237502e563c1e429b7dab305f0cc0ca8790
healer-07 <investor.jaykhun@gmail.com>
2024-11-13 23:59:45 +0300

    Add files via upload

---
+++ homework08.py
@@ -0,0 +1,11 @@
+my_list = ['banana','coconut','cherry','raspberry','lemon','lime','avocado','peach','apple','quince']
+print(my_list)
+print(my_list [0],my_list[-1])
+print(my_list [3:5])
+my_list[2] = 'grape'
+print(my_list)
+my_dict = {'wide':'широкий','deep':'глубокий','large':',большой','broad':'широкий','heavy':'тяжелый'}
+print(my_dict)
+my_dict['large'] = 'крупный'
+my_dict['narrow'] = 'узкий'
+print(my_dict)
